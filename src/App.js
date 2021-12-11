1fd9b6b0d4d2ed0da992c43fa7e7257f7ea29d44
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-12-11 13:31:06 +0200

    copy hw2 files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,65 @@
-function App() {
-  return <div>Test</div>;
+import React, { Component } from 'react';
+import Contacts from './components/Contacts/Contacts';
+import Form from './components/Form/Form';
+import Section from './components/Section/Section';
+import Filter from './components/Filter/Filter';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  getNewContact = newContact => {
+    const { contacts } = this.state;
+
+    if (contacts.find(contact => contact.name === newContact.name)) {
+      alert(`${newContact.name} is already in contacts.`);
+    } else {
+      this.setState({ contacts: [newContact, ...contacts] });
+    }
+  };
+
+  removeContact = id => {
+    const { contacts } = this.state;
+    this.setState({
+      contacts: [...contacts].filter(contact => contact.id !== id),
+    });
+  };
+
+  handleFilter = e => {
+    const { value } = e.target;
+    this.setState({ filter: value });
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+
+    const normalizeFilter = filter.toLowerCase();
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizeFilter),
+    );
+
+    return (
+      <>
+        <Section title="Phonebook">
+          <Form onSubmitContact={this.getNewContact} />
+        </Section>
+        <Section title="Contacts">
+          <Filter value={filter} onChange={this.handleFilter} />
+          <Contacts
+            contacts={filteredContacts}
+            onRemoveClick={this.removeContact}
+          />
+        </Section>
+      </>
+    );
+  }
 }
 
 export default App;
